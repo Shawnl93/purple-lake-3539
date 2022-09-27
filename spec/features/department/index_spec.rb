af039d471e807e8c42bd329ca760a5828acc7e44
Shawn Lee <Shawnl93@hotmail.com>
2022-09-27 09:44:27 -0600

    feat: shows employee's name by department

--- spec/features/department/index_spec.rb
+++ spec/features/department/index_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'department index page' do
     @department_3 = Department.create!(name: "Sales", floor: "Top")
     @employee_1 = @department_1.employees.create!(name: "Christina Aguilera", level: 2)
     @employee_2 = @department_2.employees.create!(name: "Brad pitt", level: 3)
-    @employee_2 = @department_3.employees.create!(name: "Angelina Jolie", level: 3)
+    @employee_3 = @department_3.employees.create!(name: "Angelina Jolie", level: 3)
     @ticket_1 = Ticket.create!(subject: "printers broken", age: 5, open: true)
 
   end
