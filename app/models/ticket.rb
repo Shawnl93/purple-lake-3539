8f9b57da6b77c1e233c12995724616abeb0a3ac0
Shawn Lee <Shawnl93@hotmail.com>
2022-09-27 10:21:42 -0600

    feat: sort_by_age method completed

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,5 +2,7 @@ class Ticket < ApplicationRecord
   has_many :employeetickets
   has_many :employees, through: :employeetickets
 
-
+  def self.sort_by_age
+    Ticket.where(open: true).order(age: :desc)
+  end
 end
