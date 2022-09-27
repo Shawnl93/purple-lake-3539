f49020cb95503fe6bbf5c406a020fb86e33c4c7f
Shawn Lee <Shawnl93@hotmail.com>
2022-09-27 09:43:36 -0600

    test: name of employees

--- spec/features/department/index_spec.rb
+++ spec/features/department/index_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe 'department index page' do
     @department_2 = Department.create!(name: "Accountant", floor: "Mid")
     @department_3 = Department.create!(name: "Sales", floor: "Top")
     @employee_1 = @department_1.employees.create!(name: "Christina Aguilera", level: 2)
+    @employee_2 = @department_2.employees.create!(name: "Brad pitt", level: 3)
+    @employee_2 = @department_3.employees.create!(name: "Angelina Jolie", level: 3)
     @ticket_1 = Ticket.create!(subject: "printers broken", age: 5, open: true)
 
   end
@@ -20,5 +22,12 @@ RSpec.describe 'department index page' do
     expect(page).to have_content("Department: #{@department_3.name}")
     expect(page).to have_content("Floor: #{@department_3.floor}")
 
+  end
+
+  it "underneath each department i can see the name of all of its employee" do
+    visit "/departments"
+    expect(page).to have_content(@employee_1.name)
+    expect(page).to have_content(@employee_2.name)
+    expect(page).to have_content(@employee_3.name)
   end
 end
